997d053a501e386b840aacba1b89e064cfe918d8
David Pérez <dp@incubout.com>
2016-12-19 22:11:08 +0100

    Update development

---
+++ Bomberman/src/md/games/bomberman/object/Player.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package md.games.bomberman.object;
+
+import java.awt.Graphics2D;
+import java.util.HashMap;
+import md.games.bomberman.io.GameDataLoader;
+import md.games.bomberman.io.GameDataSaver;
+import nt.lpl.types.LPLValue;
+
+/**
+ *
+ * @author David
+ */
+public class Player extends GameObject {
+    
+    private String name;
+    private Enum color;
+    private HashMap<T> position;
+
+    @Override
+    public void update(double delta) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void innserSerialize(GameDataSaver gds) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void innerUnserialize(GameDataLoader gdl) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected LPLValue getAttribute(String key) {
+        return null;
+    }
+    
+}
---
+++ Bomberman/src/md/games/bomberman/object/PlayerColor.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package md.games.bomberman.object;
+
+/**
+ *
+ * @author David
+ */
+public enum PlayerColor {
+    white,
+    red,
+    blue,
+    black,
+    green,
+    yellow;
+}
